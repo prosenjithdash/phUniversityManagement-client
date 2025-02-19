00f0369af521ac63de5e59c9e1351db04532f019
Prosenjith Dash Pappu <prosenjith.edu@gmail.com>
2025-02-20 02:33:42 +0600

    Authentication work done

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,7 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
-import About from "../pages/About";
-import Contact from "../pages/Contact";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import adminPaths from "./admin.routes";
